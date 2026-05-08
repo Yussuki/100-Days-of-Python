294a9d6f55089430f900c76ef66ffc2411edb1f2
Yussuki <uehara_thiago@hotmail.com>
2026-05-08 13:49:41 -0300

    First commit and first day

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to your band name generator!")
+cityName = input("What city do you live in?\n")
+petName = input("What is the name of a pet you have or had?\n")
+
+print("Your band name is:\n" + cityName + " " + petName)
