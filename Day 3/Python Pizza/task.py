e9a9ef2359f807c44bd0d348baff3c00b005db15
Yussuki <uehara_thiago@hotmail.com>
2026-05-12 16:43:51 -0300

    Day Three!!!

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,27 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+def calculate_cost(size, pepperoni, extra_cheese):
+    match size:
+        case "S":
+            cost = 15
+        case "M":
+            cost = 20
+        case "L":
+            cost = 25
+        case _:
+            print("Sorry, that's not a valid size.")
+            return
+
+    if pepperoni == "Y":
+        if size == "S":
+            cost += 2
+        else:
+            cost += 3
+
+    if extra_cheese == "Y":
+        cost += 1
+
+    print(f"Your final bill is: ${cost}.")
+
+calculate_cost(size, pepperoni, extra_cheese)
--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -22,3 +22,52 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+
+
+def game_over():
+    print("Game Over x.x")
+
+def make_a_choice(options: list) -> str:
+
+    for i,option in enumerate(options):
+        print(f"{i+1} - {option}")
+
+    return input("Choice: ")
+
+def game():
+    print("Do you go left or right?")
+    if make_a_choice(["left", "right"]) != "left":
+        print("You fell into a hole...")
+        game_over()
+        return
+
+    print("You find a house, but there way is blocked by the sea.")
+    print("Do you wait or try to swim?")
+    if make_a_choice(["swim", "wait"]) != "wait":
+        print("You try to swim but is attacked by a trout...")
+        game_over()
+        return
+
+    print("After a while waiting, the sea level decreases and you get to reach the house.")
+    print("Inside the house, you find three doors, a red one, a blue one and a yellow one, which one do you try?")
+    match make_a_choice(["red", "blue", "yellow"]):
+        case "red":
+            print("You trigger a fire trap and is burned crisped!")
+            game_over()
+            return
+        case "blue":
+            print("You fell in a den of beasts and is eaten...")
+            print("Who left that hole there?..")
+            game_over()
+            return
+
+        case "yellow":
+            print("Congratulations, you found the treasure chest!!!")
+            return
+
+        case _:
+            print("While deciding, the house collapsed above you")
+            game_over()
+            return
+
+game()
