bb7efdb5621c36453202e3c4e2ab6b0536effb1d
Yussuki <uehara_thiago@hotmail.com>
2026-05-13 12:29:55 -0300

    Day five!

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,24 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+random_characters = []
 
+for i in range(nr_letters):
+    letter = random.choice(letters)
+    if random.randint(0, 1) == 0:
+        letter = letter.upper()
+    random_characters.append(letter)
+
+for i in range(nr_symbols):
+    random_characters.append(random.choice(symbols))
+
+for i in range(nr_numbers):
+    random_characters.append(random.choice(numbers))
+
+random.shuffle(random_characters)
+
+password = ""
+for character in random_characters:
+    password += character
+
+print(password)
